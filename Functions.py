68deca2197cb892f08ff3cfd639603b15cbfc4db
Minette Farrell <23566892@sun.ac.za>
2022-11-12 14:13:02 +0200

    Functions chnages

--- Functions.py
+++ Functions.py
@@ -1,6 +1,8 @@
+import numpy as np
 from scamp import Session, wait
 from scamp_extensions.pitch import Scale
 import math
+import matplotlib.pyplot as plt
 mc = 60.0
 
 session = Session()
@@ -14,14 +16,23 @@ def scale(scale_up, scale_down):
         piano.play_note(scale_down[i], 0.75, 0.25)   
 
 # Major scale starting on middle C
-scale(Scale.major(60), Scale.major(60))
+# scale(Scale.major(60), Scale.major(60))
 
 # Harmonic minor scale starting on middle C
-scale(Scale.harmonic_minor(60), Scale.harmonic_minor(60))
+# scale(Scale.harmonic_minor(60), Scale.harmonic_minor(60))
 
 # Melodic minor scale starting on middle C
-scale(Scale.melodic_minor(60), Scale.harmonic_minor(60))
+# scale(Scale.melodic_minor(60), Scale.harmonic_minor(60))
 
-for i in range(0,400):
-    piano.play_note(10 * math.sin(session.beat()) + mc, 0.75, 0.05)
-    piano.play_note(10 * math.sin(session.beat()) + mc + 10, 0.75, 0.05)
+def sin_function():
+    notes = 0.0
+    i = 0
+
+    while i < 100:
+        true_c = math.sin(session.beat()) + mc/2
+        true_g_2 = 5 * math.sin(session.beat()*2) + mc/2
+        note = true_c + true_g_2
+
+        piano.play_note(note, 0.75, 0.05)
+
+sin_function()
